978509128c6feb9277f1be285d913cc3c9ab4483
Rinoa Heartilly <kissy0227@yahoo.com.au>
2018-08-07 23:33:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Add your functions and code here
-var kitten = ["Milo", "Otis", "Garfield"];
+var kittens = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendKitten(){
-  kitten.push();
+  kittens.push();
 }
