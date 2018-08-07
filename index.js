8c22ee78747193c3b268ef11d790f8e8d616d2cd
Rinoa Heartilly <kissy0227@yahoo.com.au>
2018-08-07 23:35:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ var kittens = ["Milo", "Otis", "Garfield"];
 function destructivelyAppendKitten(name){
   kittens.push(name);
 }
+
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+
+array
